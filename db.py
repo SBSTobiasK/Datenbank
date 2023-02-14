d18d4b2b5c52c9beddcb37ab0dfa5365dd4ebe59
sbstobiask <sbs202223kerchner@gmail.com>
2023-02-14 10:57:07 +0100

    1

--- db.py
+++ db.py
@@ -18,5 +18,36 @@ class DB:
         ''')
         self.__connection.commit()
 
-    def customer_add(self, vorname, nachname, email):
-        pass
+    def customer_add(self, vorname, nachname):
+        self.__corsor.execute('''
+        SELECT MAX (pk) FROM customer;
+        ''')
+        pk = 0
+        ret = self.__corsor.fetchall()
+        ret_tupel = ret[0]
+        ret = ret_tupel[0]
+        if ret == None:
+            pk = 1
+        else:
+            pk = ret+1
+        self.__corsor.execute('''
+        INSERT INTO customer
+        VALUES  ("{}", "{}", "{}");
+        '''.format(pk, vorname, nachname))
+        self.__connection.commit()
+        return pk
+
+    def getCustomer(self):
+        self.__corsor.execute('''
+        SELECT * FROM customer;
+        ''')
+        ret = self.__corsor.fetchall()
+        return ret
+
+    def getCustomerInfo(self, pk):
+        self.__corsor.execute('''
+                SELECT * FROM customer
+                WHERE pk = {};
+                '''.format(pk))
+        ret = self.__corsor.fetchall()
+        return ret
--- testbench.py
+++ testbench.py
@@ -1,3 +1,9 @@
 from db import *
 
 db = DB()
+ret = db.customer_add("Testvorname", "Testnachname")
+print(ret)
+ret = db.getCustomer()
+print(ret)
+ret = db.getCustomerInfo(1)
+print(ret)
